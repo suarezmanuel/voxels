93423107c83fab8c97be11a6e76915d48e64b014
suarezmanuel <manuelsuarez244@gmail.com>
2025-06-17 16:25:08 +0300

    where is the nerias?

---
+++ build.sh
@@ -0,0 +1,19 @@
+set -e # if a command fails, exit immediatly
+
+if [ -z "$1" ]; then
+    echo "missing flag parameter, e.g. ./rebuild.bash {MACHINE}\n'MACHINE' = MAC, WIN"
+    exit 1
+fi
+
+rm -rf build
+mkdir build
+cd build 
+cmake ..
+cmake --build .
+echo $1
+
+if [ $1 == "MAC" ]; then
+    open VoxelCube.app
+elif [ $1 == "WIN" ]; then
+    ./VoxelCube.exe
+fi
